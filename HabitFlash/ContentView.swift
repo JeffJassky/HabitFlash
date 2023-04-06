ac3ed9475b2e7c8f4cb778b12b1915334a2b5912
Jeff Jassky <jeff@jeffjassky.com>
2023-04-06 17:43:09 -0400

    Initial Commit

---
+++ HabitFlash/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HabitFlash
+//
+//  Created by Jeff Jassky on 4/6/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HabitFlash/HabitFlashApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitFlashApp.swift
+//  HabitFlash
+//
+//  Created by Jeff Jassky on 4/6/23.
+//
+
+import SwiftUI
+
+@main
+struct HabitFlashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
